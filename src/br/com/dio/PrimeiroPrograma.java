d004d1ed24f344674080b9d90d0efd6999ae842e
Alexander <alexaraujo2209@gmail.com>
2022-04-02 15:12:59 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Cat cat =new Cat();
+		Smartphones smartphones = new Smartphones();
+		
+		System.out.println(cat);
+		System.out.println(smartphones);
+		
+		/*int a = 2;
+		int b = 3;
+				System.out.println("Fala comigo mundão :D " + (a+b));*/
+	}
+
+}
+
+class Smartphones {
+	private String nome;
+	private String npag;
+	
+}
